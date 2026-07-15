ec49415e083f98c78717d1c9e1e8d46b45f1be6a
fravila08 <fr4v1l4@gmail.com>
2026-07-15 12:26:52 -0700

    Add ExperienceSection and EducationSection to HomePage
    Both render as a simple vertical timeline: role/program title,
    org/school + dates, and supporting bullets/description.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import AboutSection from '../components/AboutSection.jsx'
 import SkillsSection from '../components/SkillsSection.jsx'
+import ExperienceSection from '../components/ExperienceSection.jsx'
+import EducationSection from '../components/EducationSection.jsx'
 
 function HomePage() {
   return (
     <div>
       <AboutSection />
       <SkillsSection />
+      <ExperienceSection />
+      <EducationSection />
     </div>
   )
 }
